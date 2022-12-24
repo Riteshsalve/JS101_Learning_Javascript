69ee971bb0ee9417d19d378be76936a78db07bdd
Riteshsalve <121334201+Riteshsalve@users.noreply.github.com>
2022-12-24 22:54:52 +0530

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,7 +1,7 @@
 //problem1.js 
 Print "Masai School" in the console followed by "A Transformation in Education" in next line//
 
-let a "Masai School";
+let a = "Masai School";
 console.log(a);
 let b = "A Transformation in Education";
 console.log(b);
