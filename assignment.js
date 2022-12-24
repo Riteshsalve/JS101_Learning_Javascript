1b1eb8fc0db9c4a4768f0f064220e3867d88cfa8
ritesh salve <riteshsalve07@gmail.com>
2022-12-24 22:51:07 +0530

    js101day1

---
+++ assignment.js
@@ -0,0 +1,57 @@
+//problem1.js 
+Print "Masai School" in the console followed by "A Transformation in Education" in next line//
+
+let a "Masai School";
+console.log(a);
+let b = "A Transformation in Education";
+console.log(b);
+
+//Problem2.js
+Define a variable called name with your Name as the assigned value
+Print the value stored in the variable name
+Change the variable to store your father's name
+Print the value stored in the variable name
+Change the variable again to store your mother's name.
+Print the value stored in the variable name//
+
+
+let name = "Ritesh salve";
+console.log(name);
+let father_name = "Ravindra salve";
+console.log(father_name);
+let mother_name = "kamalbai salve";
+console.log(mother_name);
+
+//Problem3.js
+Create a variable name to store your name
+Create a variable age to store your age
+Print the values stored in the variable on one line followed by the type of the variable in the next line//
+
+let name = "Ritesh";
+let age = 25;
+
+console.log(name,age);
+console.log(typeof(name));
+console.log(typeof(age));
+
+//Problem4.js
+Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+Print the report card of the student//
+
+let name = " Name - Ritesh salve";
+let school = " School - Masai school india";
+let grade = "Grade - A";
+let section = "Section - B";
+let roll_no = "Roll_No - 07"
+console.log(name);
+console.log(school);
+console.log(grade);
+console.log(section);
+console.log(roll_no);
+let sub_1 = "Hindi = 88";
+let sub_2 = "English = 75";
+let sub_3 = "Science = 78";
+console.log(sub_1);
+console.log(sub_2);
+console.log(sub_3);
+
